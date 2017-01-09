11d98f3c0b2a178c39fce2f8c61f6b8d237191bf
Maxim Goryachev <maxto4kacom@gmail.com>
2017-01-09 02:37:52 +0200

    start jade pug

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,19 @@ var gulp        = require('gulp'),
     cssnano     = require('gulp-cssnano'),
     rename      = require('gulp-rename'),
     del         = require('del'),
-    cache       = require('gulp-cache');
+    cache       = require('gulp-cache'),
+    jadepug     = require('gulp-pug');
 
 gulp.task('sass', function () {
     return gulp.src('site/sass/*.+(sass|scss)')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('site/css'))
+});
+
+gulp.task('jade-pug', function () {
+    return gulp.src('site/jade/pug/*.pug')
+        .pipe(jadepug({pretty: true}))
+        .pipe(gulp.dest('site/jade'))
 });
 
 gulp.task('min', ['sass'], function () {
@@ -26,6 +33,7 @@ gulp.task('clean-prod', function () {
 gulp.task('watch', function () {
     gulp.watch('site/sass/**/*.+(sass|scss)', ['sass']);
     gulp.watch('site/css/main.css', ['min']);
+    gulp.watch('site/jade/pug/*.pug', ['jade-pug']);
 });
 
 gulp.task('fill', ['clean-prod', 'min'], function () {
